3f0be98bf521759b63a017be8a9f36620508e0c7
D34DPlayer <d34dplayer@protonmail.com>
2020-12-25 17:26:49 +0100

    new: download db fields
    + logging config

--- MuziekDL/models.py
+++ MuziekDL/models.py
@@ -1,9 +1,19 @@
+import os.path
+
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
 
 class SongEntry(models.Model):
+    DL_STATUS = [
+        (0, "None"),
+        (1, "Pending"),
+        (2, "Done"),
+        (-1, "Error"),
+    ]
+
     song_identifier = models.CharField(max_length=32)
     album = models.CharField(max_length=64, blank=True)
     artist = models.CharField(max_length=64, blank=True)
@@ -14,3 +24,7 @@ class SongEntry(models.Model):
     track_title = models.CharField(max_length=128, blank=True)
     year = models.IntegerField("year published", blank=True, null=True)
     add_date = models.DateTimeField("date entry added")
+    dl_status = models.IntegerField(default=0, choices=DL_STATUS)
+    dl_file = models.FilePathField(path=settings.SONGS_PATH, blank=True, null=True)
+    dl_error = models.CharField(max_length=64, blank=True)
+
--- MuziekDownloader/settings.py
+++ MuziekDownloader/settings.py
@@ -120,3 +120,41 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SONGS_PATH = './songs/'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        }
+    }
+}
